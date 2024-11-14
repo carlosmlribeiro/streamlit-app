06901547f7b82b7e855b225bdb4210e8c71c4336
Carlos Ribeiro <carlosmlribeiro@gmail.com>
2024-11-14 16:49:52 +0000

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from langchain_openai.chat_models import ChatOpenAI
+
+st.title("Chatbot")
+
+#openai_api_key = st.sidebar.text_input("openai API key", type="password")
+
+openai_api_key = st.secrets["OPENAI_API_KEY"]
+
+def generate_response(input_text):
+    model = ChatOpenAI(temperature=0, api_key=openai_api_key)
+    st.info(model.invoke(input_text))
+
+with st.form("my_form"):
+    text = st.text_area(
+        "Enter text:",
+        "Who is Lewis Hamilton?",
+    )
+    submitted = st.form_submit_button("Submit")
+    if not openai_api_key.startswith("sk-"):
+        st.warning("Invalid API key")
+    if submitted and openai_api_key.startswith("sk-"):
+        generate_response(text)
